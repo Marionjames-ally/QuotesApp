8d455044db1e81f89e09d7ddbeacf95aa0b2dce1
marionjames <maarionjames2018@gmail.com>
2019-10-27 09:43:53 +0300

    modify the constructer function in the quote.ts file

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,6 @@
 export class Quote {
-    public id :number;
-    public quote :string;
-    public name :string;
-    public postDate :Date;
-    public upVote :number;
+    public showInfo: boolean;
+    constructor(public id :number, public quote :string, public name :string, public postDate :Date, public upVote :number){
+        this.showInfo = false;
+    }
 }
