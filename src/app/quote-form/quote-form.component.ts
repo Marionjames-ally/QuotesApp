e0e99fbc75d0fddab6658f40f37c301c4171636c
marionjames <maarionjames2018@gmail.com>
2019-10-28 11:51:33 +0300

    fix bug in quote-form.component.ts file

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
-import { Quote } from '@angular/compiler';
-import { EventEmitter } from 'events';
-
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
   styleUrls: ['./quote-form.component.css']
 })
-export class QuoteFormComponent implements OnInit {
-
+export class QuoteFormComponent implements OnInit {  
   @Input() quote: Quote;
-  @Output() inputQuote = new EventEmitter(<Quote>);
+  @Output() inputQuote = new EventEmitter<Quote>();
 
-  newQuote = new Quote(0,'', '',new Date(0,0,0) , 0, 0);
+  newQuote = new Quote(0,'', '',new Date(0,0,0), 0, 0);
   submitForm(){
     this.inputQuote.emit(this.newQuote);
   }
-
   constructor() { }
-
+  
   ngOnInit() {
   }
 
