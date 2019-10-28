7506c7298b4a9e03e4635c9881dbc0f90eb6e3a7
marionjames <maarionjames2018@gmail.com>
2019-10-28 15:25:14 +0300

    remove components and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
+import { QuoteFormComponent } from "./quote-form/quote-form.component";
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DateCountPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/quote.ts
+++
@@ -1,8 +0,0 @@
-export class Quote {
-    public showInfo: boolean;
-    constructor(public id :number, public quote :string, public name :string, public postDate :Date, public downVote :number, public upVote :number){
-        this.showInfo = false;
-    }
-
-    
-}
