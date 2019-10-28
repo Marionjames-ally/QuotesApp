d07087a0509ad8c766c369f0590b997b8554fab8
marionjames <maarionjames2018@gmail.com>
2019-10-28 11:29:12 +0300

    modify quote-form.component.ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 import { Quote } from '@angular/compiler';
 import { EventEmitter } from 'events';
 
+
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
@@ -10,7 +11,7 @@ import { EventEmitter } from 'events';
 export class QuoteFormComponent implements OnInit {
 
   @Input() quote: Quote;
-  @Output() inputQuote = new EventEmitter();
+  @Output() inputQuote = new EventEmitter(<Quote>);
 
   newQuote = new Quote(0,'', '',new Date(0,0,0) , 0, 0);
   submitForm(){
